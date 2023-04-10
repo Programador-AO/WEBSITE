d5f8e146735593a6b1026d2e43b4f7999f213026
Pedro Edilásio Araújo Lopes <edilasio@live.com>
2023-04-10 12:20:30 +0100

    section: O caminho para ser um programador de sucesso

--- index.php
+++ index.php
@@ -11,21 +11,53 @@
         </div>
     </section>
 
-    <!-- <div class="publicidade">
-        <h1>Faça parte da maior plataforma de <br> programadores angolanos</h1>
-        <h3>
-            Se voce é um programador angolano em busca de uma plataforma vibrante e colaborativa, voce está no lugar
-            certo junte-se a nós e faça parte da maior plataforma de programadores do pais
-            <p> <br> </p>
-            Ao se juntar à nós, você terá acesso a uma ampla gama de recursos, incluindo fóruns de discussão, grupos
-            de estudo, eventos
-            e oportunidades de networking. Além disso, você terá a chance de colaborar em projetos de código aberto
-            e contribuir para a
-            criação de soluções inovadoras para desafios tecnológicos.
-        </h3>
-        <button type="submit">Saber mais</button>
-    </div>
-    <div class="publicidade2">
+    <section class="sobre-nos">
+        <h1 class="title">Faça parte da maior plataforma de programadores angolanos</h1>
+        <p class="text">
+            Se você é um programador angolano em busca de uma plataforma vibrante e colaborativa, você está no lugar certo! Junte-se a nós e faça parte da maior plataforma de programadores do país.
+            </br>
+            </br>
+            Ao se juntar à nós, você terá acesso a uma ampla gama de recursos, incluindo fóruns de discussão, grupos de estudo, eventos e oportunidades de networking. Além disso, você terá a chance de colaborar em projetos de código aberto e contribuir para a criação de soluções inovadoras para desafios tecnológicos.
+        </p>
+        <a href="./sobre-nos.php" class="button-primary-flat">Saber mais</a>
+    </section>
+
+    <section class="caminho-sucesso">
+        <h1 class="title">O caminho para ser um programador de sucesso</h1>
+        <div class="cards">
+            <div class="card">
+                <svg width="167" height="167" viewBox="0 0 167 167" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M150.3 83.5C150.3 46.6682 120.332 16.7 83.5 16.7C46.6682 16.7 16.7 46.6682 16.7 83.5C16.7 120.332 46.6682 150.3 83.5 150.3C120.332 150.3 150.3 120.332 150.3 83.5ZM167 83.5C167 129.617 129.617 167 83.5 167C37.383 167 0 129.617 0 83.5C0 37.383 37.383 0 83.5 0C129.617 0 167 37.383 167 83.5ZM133.6 91.85C133.6 96.4592 129.859 100.2 125.25 100.2H91.85C82.6233 100.2 75.15 92.7268 75.15 83.5V41.75C75.15 37.1408 78.8908 33.4 83.5 33.4C88.1092 33.4 91.85 37.1408 91.85 41.75V75.15C91.85 79.7425 95.6075 83.5 100.2 83.5H125.25C129.859 83.5 133.6 87.2408 133.6 91.85Z" fill="#9103D5" />
+                </svg>
+
+                <h2>Dedicação</h2>
+                <p>Você deve estar disposto a dedicar seu tempo e energia para aprender novas habilidades e tecnologias, aprimorar suas habilidades existentes e trabalhar em projetos desafiadores.</p>
+            </div>
+            <div class="card">
+                <svg width="170" height="170" viewBox="0 0 170 170" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M44.4004 111.48L72.9608 81.8801C74.5608 80.2001 77.2008 80.2801 78.7208 81.9601L85.12 88.8401C86.72 90.5201 89.3608 90.5201 90.9608 88.8401L107.52 71.4801" stroke="#9103D5" stroke-width="10" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M123.04 87.88L123.6 60.28C123.6 57.8 121.601 55.72 119.201 55.72L91.4408 55.88" stroke="#9103D5" stroke-width="10" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M85 161C126.974 161 161 126.974 161 85C161 43.0263 126.974 9 85 9C43.0263 9 9 43.0263 9 85C9 126.974 43.0263 161 85 161Z" stroke="#9103D5" stroke-width="17" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+
+
+                <h2>Persistência</h2>
+                <p>A programação pode ser difícil e frustrante às vezes, especialmente quando você está tentando resolver um problema complicado. É nesses momentos que a persistência se torna importante.</p>
+            </div>
+            <div class="card">
+                <svg width="170" height="170" viewBox="0 0 170 170" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M84.875 160.875C126.849 160.875 160.875 126.849 160.875 84.875C160.875 42.9014 126.849 8.875 84.875 8.875C42.9014 8.875 8.875 42.9014 8.875 84.875C8.875 126.849 42.9014 160.875 84.875 160.875Z" stroke="#9103D5" stroke-width="17" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M84.9594 118.653H84.875V118.737H84.9594V118.653Z" stroke="#9103D5" stroke-width="10" stroke-linejoin="round" />
+                    <path d="M72.9329 47.5988C76.2228 44.3087 80.5329 42.6598 84.8454 42.6522C89.1774 42.6446 93.512 44.2935 96.8171 47.5988C100.115 50.8966 101.764 55.2188 101.764 59.5411C101.764 63.8633 100.115 68.1854 96.8171 71.4829C93.512 74.7889 89.1774 76.4373 84.8454 76.4297L84.875 84.8741" stroke="#9103D5" stroke-width="10" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+
+                <h2>Curiosidade</h2>
+                <p>A programação é uma área em constante evolução, e há sempre algo novo para aprender e explorar. Ao ser curioso, você pode descobrir novas abordagens, tecnologias e técnicas que podem ajudar a melhorar.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- <div class="publicidade2">
         <div class="title">
             <h1>O caminho para ser um programador <br> de sucesso</h1>
         </div>
