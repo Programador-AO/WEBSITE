56825a14c52ce17ab87a1fcadffd99543efdd092
Pedro Edilásio Araújo Lopes <edilasio@live.com>
2023-04-11 10:54:00 +0100

    home web concluído

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         </div>
     </section>
 
-    <section class="sobre-nos">
+    <section class="section bg-secondaryB">
         <h1 class="title">Faça parte da maior plataforma de programadores angolanos</h1>
         <p class="text">
             Se você é um programador angolano em busca de uma plataforma vibrante e colaborativa, você está no lugar certo! Junte-se a nós e faça parte da maior plataforma de programadores do país.
@@ -22,141 +22,84 @@
         <a href="./sobre-nos.php" class="button-primary-flat">Saber mais</a>
     </section>
 
-    <section class="caminho-sucesso">
+    <section class="section">
         <h1 class="title">O caminho para ser um programador de sucesso</h1>
         <div class="cards">
-            <div class="card">
-                <svg width="167" height="167" viewBox="0 0 167 167" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div class="card bg-secondaryB zoom p-32 w-324 flex-col">
+                <svg class="card-svg" width="167" height="167" viewBox="0 0 167 167" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M150.3 83.5C150.3 46.6682 120.332 16.7 83.5 16.7C46.6682 16.7 16.7 46.6682 16.7 83.5C16.7 120.332 46.6682 150.3 83.5 150.3C120.332 150.3 150.3 120.332 150.3 83.5ZM167 83.5C167 129.617 129.617 167 83.5 167C37.383 167 0 129.617 0 83.5C0 37.383 37.383 0 83.5 0C129.617 0 167 37.383 167 83.5ZM133.6 91.85C133.6 96.4592 129.859 100.2 125.25 100.2H91.85C82.6233 100.2 75.15 92.7268 75.15 83.5V41.75C75.15 37.1408 78.8908 33.4 83.5 33.4C88.1092 33.4 91.85 37.1408 91.85 41.75V75.15C91.85 79.7425 95.6075 83.5 100.2 83.5H125.25C129.859 83.5 133.6 87.2408 133.6 91.85Z" fill="#9103D5" />
                 </svg>
 
-                <h2>Dedicação</h2>
-                <p>Você deve estar disposto a dedicar seu tempo e energia para aprender novas habilidades e tecnologias, aprimorar suas habilidades existentes e trabalhar em projetos desafiadores.</p>
+                <h2 class="card-h2">Dedicação</h2>
+                <p class="card-p">Você deve estar disposto a dedicar seu tempo e energia para aprender novas habilidades e tecnologias, aprimorar suas habilidades existentes e trabalhar em projetos desafiadores.</p>
             </div>
-            <div class="card">
-                <svg width="170" height="170" viewBox="0 0 170 170" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div class="card bg-secondaryB zoom p-32 w-324 flex-col">
+                <svg class="card-svg" width="170" height="170" viewBox="0 0 170 170" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M44.4004 111.48L72.9608 81.8801C74.5608 80.2001 77.2008 80.2801 78.7208 81.9601L85.12 88.8401C86.72 90.5201 89.3608 90.5201 90.9608 88.8401L107.52 71.4801" stroke="#9103D5" stroke-width="10" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M123.04 87.88L123.6 60.28C123.6 57.8 121.601 55.72 119.201 55.72L91.4408 55.88" stroke="#9103D5" stroke-width="10" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M85 161C126.974 161 161 126.974 161 85C161 43.0263 126.974 9 85 9C43.0263 9 9 43.0263 9 85C9 126.974 43.0263 161 85 161Z" stroke="#9103D5" stroke-width="17" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
 
-
-                <h2>Persistência</h2>
-                <p>A programação pode ser difícil e frustrante às vezes, especialmente quando você está tentando resolver um problema complicado. É nesses momentos que a persistência se torna importante.</p>
+                <h2 class="card-h2">Persistência</h2>
+                <p class="card-p">A programação pode ser difícil e frustrante às vezes, especialmente quando você está tentando resolver um problema complicado. É nesses momentos que a persistência se torna importante.</p>
             </div>
-            <div class="card">
-                <svg width="170" height="170" viewBox="0 0 170 170" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div class="card bg-secondaryB zoom p-32 w-324 flex-col">
+                <svg class="card-svg" width="170" height="170" viewBox="0 0 170 170" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M84.875 160.875C126.849 160.875 160.875 126.849 160.875 84.875C160.875 42.9014 126.849 8.875 84.875 8.875C42.9014 8.875 8.875 42.9014 8.875 84.875C8.875 126.849 42.9014 160.875 84.875 160.875Z" stroke="#9103D5" stroke-width="17" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M84.9594 118.653H84.875V118.737H84.9594V118.653Z" stroke="#9103D5" stroke-width="10" stroke-linejoin="round" />
                     <path d="M72.9329 47.5988C76.2228 44.3087 80.5329 42.6598 84.8454 42.6522C89.1774 42.6446 93.512 44.2935 96.8171 47.5988C100.115 50.8966 101.764 55.2188 101.764 59.5411C101.764 63.8633 100.115 68.1854 96.8171 71.4829C93.512 74.7889 89.1774 76.4373 84.8454 76.4297L84.875 84.8741" stroke="#9103D5" stroke-width="10" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
 
-                <h2>Curiosidade</h2>
-                <p>A programação é uma área em constante evolução, e há sempre algo novo para aprender e explorar. Ao ser curioso, você pode descobrir novas abordagens, tecnologias e técnicas que podem ajudar a melhorar.</p>
+                <h2 class="card-h2">Curiosidade</h2>
+                <p class="card-p">A programação é uma área em constante evolução, e há sempre algo novo para aprender e explorar. Ao ser curioso, você pode descobrir novas abordagens, tecnologias e técnicas que podem ajudar a melhorar.</p>
             </div>
         </div>
     </section>
 
-    <!-- <div class="publicidade2">
-        <div class="title">
-            <h1>O caminho para ser um programador <br> de sucesso</h1>
+    <section class="section bg-secondaryB">
+        <h1 class="title">Projectos desenvolvidos pela comunidade</h1>
+        <p class="text">Esses projetos são criados e desenvolvidos em conjunto pelos membros da comunidade e podem ter diferentes objetivos e escopos, desde soluções para problemas locais até soluções de escala global.</p>
+        <div class="cards">
+            <a href="" class="card border-gray hover p-32 w-324">
+                <h3 class="card-h3">Dedicação</h2>
+            </a>
+            <a class="card border-gray hover p-32 w-324">
+                <h3 class="card-h3">Persistência</h2>
+            </a>
+            <a class="card border-gray hover p-32 w-324">
+                <h3 class="card-h3">Curiosidade</h2>
+            </a>
         </div>
-        <div class="row">
-            <div class="card">
-                <img src="imagens/Page-1.png" alt="">
-                <h1>Dedicação</h1>
-                <p>Você deve estar disposto a dedicar seu tempo e energia para aprender novas habilidades e
-                    tecnologias, aprimorar suas habilidades xistentes e trabalhar em projetos desafiadores.</p>
-            </div>
-            <div class="card">
-                <img src="imagens/progress_svgrepo.com.png" alt="">
-                <h1>Persistência</h1>
-                <p>
-                    A programação pode ser difícil e frustrante às vezes, especialmente quando você está tentando
-                    resolver um problema complicado. É nesses momentos que a persistência se torna importante.</p>
-            </div>
-            <div class="card">
-                <img src="imagens/question-mark-circle_svgrepo.com.png" alt="">
-                <h1>Curiosidade</h1>
-                <p>A programação é uma área em constante
-                    evolução, e há sempre algo novo para
-                    aprender e explorar. Ao ser curioso, você pode descobrir novas abordagens, tecnologias e
-                    técnicas que podem ajudar a melhorar.</p>
-            </div>
+        <a href="./projectos.php" class="button-primary-flat mt-28">Ver Todos</a>
+    </section>
 
-
-        </div>
-
-    </div>
-    <div class="publicidade3">
-        <div class="title">
-            <h1>Projectos desenvolvidos pela <br> comunidade</h1>
-
-            <p>Esses projetos são criados e desenvolvidos em conjunto pelos membros da comunidade e podem ter
-                diferentes objetivos e escopos, desde soluções para problemas locais até soluções de escala global.
-            </p>
-        </div>
-        <div class="row">
-            <div class="card">
-                <h2>Perdidos e achados de documentos</h2>
-            </div>
-            <div class="card">
-                <h2>Perdidos e achados de documentos</h2>
-            </div>
-            <div class="card">
-                <h2>Perdidos e achados de documentos</h2>
-            </div>
-        </div>
-        <button type="submit">Ver Todos</button>
-    </div>
-    <div class="banner">
-        <div class="one">
-            <img src="imagens/phone.png" alt="phone">
-            <div class="detail">
-                <p>Aplicativos para</p>
-                <h1>Android iOS</h1>
-                <p>Baixe e vêja nossos conteúdos</p>
-            </div>
-        </div>
-        <div class="two">
-            <img src="imagens/googleplay.png" alt="googleplay">
-            <img src="imagens/appStore.png" alt="appStore">
-        </div>
-    </div>
-    <div class="publicidade4">
-        <div class="row">
-            <div class="card" id="first">
-                <img src="imagens/logo (1).png" alt="logo">
-                <div class="detail">
-                    <h1>Assine a nossa newsletter</h1>
-                    <p>Obtenha atualizações de projectos, notícias e muito mais.</p>
-                </div>
-                <button type="submit">
-                    INSCREVER-SE
-                </button>
-            </div>
-            <div class="card">
-                <h1>
-                    Contacto
+    <section class="bg-primary py-24">
+        <div class="flex between">
+            <div class="flex center">
+                <svg width="58" height="93" viewBox="0 0 58 93" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M49.3 0H8.7C3.89688 0 0 3.90527 0 8.71875V84.2812C0 89.0947 3.89688 93 8.7 93H49.3C54.1031 93 58 89.0947 58 84.2812V8.71875C58 3.90527 54.1031 0 49.3 0ZM29 87.1875C25.7919 87.1875 23.2 84.59 23.2 81.375C23.2 78.16 25.7919 75.5625 29 75.5625C32.2081 75.5625 34.8 78.16 34.8 81.375C34.8 84.59 32.2081 87.1875 29 87.1875ZM49.3 67.5703C49.3 68.7691 48.3213 69.75 47.125 69.75H10.875C9.67875 69.75 8.7 68.7691 8.7 67.5703V10.8984C8.7 9.69961 9.67875 8.71875 10.875 8.71875H47.125C48.3213 8.71875 49.3 9.69961 49.3 10.8984V67.5703Z" fill="#E1E1E6" />
+                </svg>
+                <h1 class="ml-24 title flex-col ">
+                    <span class="text-20">Aplicativos para</span>
+                    Android e iOS
+                    <span class="text-20">Baixe e veja nossos conteúdos</span>
                 </h1>
-                <p>dev@programador.ao</p>
-                <p> contributo@programador.ao</p>
-                <p> doacao@programador.ao</p>
-
             </div>
-            <div class="card">
-                <h1>Projectos</h1>
-                <p>Eventos</p>
-                <p>API do Desenvolvedor</p>
-                <p>Blog</p>
-            </div>
-            <div class="card">
-                <h1>Suporte</h1>
-                <p>omunidade</p>
-                <p>Central de ajuda</p>
-                <p>Sobre nós</p>
+            <div class="flex center cards">
+                <a href="#googlepla" class="flex card bg-white py-12 px-24 zoom">
+                    <svg width="38" height="42" viewBox="0 0 38 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M24.6142 19.2237L6.50635 1.06661L29.5452 14.2926L24.6142 19.2237ZM1.78043 0C0.713811 0.557921 0 1.57531 0 2.89627V39.1037C0 40.4247 0.713811 41.4421 1.78043 42L22.8338 20.9959L1.78043 0ZM36.6669 18.5099L31.8343 15.7121L26.4438 21.0041L31.8343 26.2962L36.7654 23.4983C38.2422 22.3251 38.2422 19.6831 36.6669 18.5099ZM6.50635 40.9416L29.5452 27.7156L24.6142 22.7845L6.50635 40.9416Z" fill="#10121F" />
+                    </svg>
+                    <h1 class="text-32 secondary ml-24">Google Play</h1>
+                </a>
+                <a href="#playstore" class="flex card bg-white py-12 px-24 zoom">
+                    <svg width="35" height="42" viewBox="0 0 35 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M29.2628 22.1947C29.2442 18.7534 30.7877 16.1561 33.9121 14.2432C32.1639 11.7209 29.5231 10.3331 26.0361 10.0612C22.7351 9.79866 19.1273 12.0022 17.8069 12.0022C16.4121 12.0022 13.2133 10.155 10.7027 10.155C5.51408 10.2394 0 14.3276 0 22.6447C0 25.1014 0.446334 27.6394 1.339 30.2586C2.52922 33.6999 6.82519 42.1389 11.3071 41.9983C13.6504 41.942 15.3055 40.3198 18.3555 40.3198C21.3124 40.3198 22.8467 41.9983 25.4596 41.9983C29.9787 41.9326 33.8656 34.2625 35 30.8119C28.9373 27.9332 29.2628 22.3728 29.2628 22.1947ZM23.9997 6.79811C26.5383 3.76006 26.3058 0.993931 26.2314 0C23.9904 0.131274 21.3961 1.53778 19.9176 3.27247C18.2904 5.12906 17.3326 7.42635 17.5372 10.0143C19.9641 10.2019 22.1772 8.94538 23.9997 6.79811Z" fill="#10121F" />
+                    </svg>
+                    <h1 class="text-32 secondary ml-24">Apple Store</h1>
+                </a>
             </div>
         </div>
-    </div> -->
+    </section>
 </div>
 <?php include './includes/footer.php'; ?>
